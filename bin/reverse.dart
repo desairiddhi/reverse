59b6f267eea35ee80093ab796a6c17aeae01240e
Desai Riddhi <desairiddhi15@gmail.com>
2022-03-31 11:23:49 +0530

    first commit

---
+++ bin/reverse.dart
@@ -0,0 +1,24 @@
+import'dart:io';
+void main(){
+    int reverse=0;
+    List<int> array=[];
+
+    stdout.write("enter the size of array:");
+    int? n=int.parse(stdin.readLineSync()!);
+
+    stdout.write("enter the elements of array:");
+    for(int i=0;i<n;++i){
+      int? reverse=int.parse(stdin.readLineSync()!);
+        array.add(reverse);
+    }
+   // print("revesre number:");
+    for(var i=0;i<array.length/2;i++){
+      reverse=array[i];
+      array[i]=array[array.length-1-i];
+      array[array.length-1-i]=reverse;}
+
+      print("revesre number:");
+      for(int i=0;i<n;++i){
+      print(array[i]);
+      }
+}
